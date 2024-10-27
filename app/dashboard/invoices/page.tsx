4210775bbbc841c8b5ecc1e450523c2c00cd6d8b
Virendra Nishad <virendran818@gmail.com>
2024-10-27 23:19:54 +0530

    invoice crud done

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,8 @@ import { inter } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
+import { fetchInvoicesPages } from '@/app/lib/data';
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
@@ -15,6 +17,8 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -29,7 +33,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
